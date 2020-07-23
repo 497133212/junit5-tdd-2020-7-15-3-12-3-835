baf620892dc7498f3e9e15fd0635a28bd769b503
Austin <497133212@qq.com>
2020-07-23 11:54:31 +0800

    [FizzBuzzWhizz][Austin] feature: add should_return_FizzBuzzWhizz_when_play_fizz_buzz_whizz_given_105 test

--- src/test/java/junit5/tdd/FizzBuzzWhizzTest.java
+++ src/test/java/junit5/tdd/FizzBuzzWhizzTest.java
@@ -71,4 +71,17 @@ public class FizzBuzzWhizzTest {
         //then
         assertEquals("FizzBuzz", actual);
     }
+
+    @Test
+    void should_return_FizzBuzzWhizz_when_play_fizz_buzz_whizz_given_105() {
+        //given
+        int number =105;
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+
+        //when
+        String actual = fizzBuzzWhizz.play(number);
+
+        //then
+        assertEquals("FizzBuzzWhizz", actual);
+    }
 }
